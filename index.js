f39999f1dfa464a0cf21eb3b1377ccc88c2beceb
ArtemFilipovich42 <105447122+ArtemFilipovich42@users.noreply.github.com>
2022-06-01 20:27:25 +0300

    jsuser

---
+++ index.js
@@ -0,0 +1,8 @@
+var name1 = prompt('Как тебя зовут?!');
+var surname = prompt('Твоя Фамилмя?!');
+var cifr = prompt('Сколько тебе лет?');
+if (+cifr > 21) {
+    alert(`${name1} ${surname} Привет`)} 
+    else {
+ var cifr2 = prompt('Сколько тебе лет?')
+ alert (`${name1} ${surname} ${cifr2} Привет`)}
