a8810e1cb1e4f7d4eb0d6ab32744d4f1a4d8fc2a
ArtemFilipovich42 <105447122+ArtemFilipovich42@users.noreply.github.com>
2022-06-05 10:52:25 +0300

    zadanie

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
-var name1 = prompt('Как тебя зовут?!');
-var surname = prompt('Твоя Фамилмя?!');
-var cifr = prompt('Сколько тебе лет?');
-if (+cifr > 21) {
-    alert(`${name1} ${surname} Привет`)} 
-    else {
- var cifr2 = prompt('Сколько тебе лет?')
- alert (`${name1} ${surname} ${cifr2} Привет`)}
+var name1 = prompt('enter the text!');
+var cifr = name1.length;
+var i = 0;
+var sumA;
+var sumB;
+var sumE;
+var sumO;
+function aAll(sumA) {
+    for (i = 0; i <= cifr; i++) {
+        var a = name1[i]
+        if (a === 'a') {
+            sumA++
+        }
+
+    }
+    return (console.log(sumA))
+}
+function bAll(sumB) {
+    for (i = 0; i <= cifr; i++) {
+        var b = name1[i]
+        if (b === 'b') {
+            sumB++
+        }
+
+    }
+    return (console.log(sumB))
+}
+
+function oAll(sumO) {
+    for (i = 0; i <= cifr; i++) {
+        var o = name1[i]
+        if (o === 'o') {
+            sumO++
+        }
+    }
+    return (console.log(sumO))
+}
+function eAll(sumE) {
+    for (i = 0; i <= cifr; i++) {
+        var e = name1[i]
+        if (e === 'e') {
+            sumE++
+        }
+
+    }
+    return (console.log(sumA))
+}
+
+
+
+
+
